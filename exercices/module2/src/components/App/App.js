a75dc88a6e71d543cbab05d401d81280ad00a67e
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-09-30 11:52:53 +0200

    module2 : localStorage

--- exercices/module2/src/components/App/App.js
+++ exercices/module2/src/components/App/App.js
@@ -1,31 +1,26 @@
-import { useState } from "react"
-import Display from "components/Display/Display"
-import Button from "components/Button/Button"
+import { useState } from "react";
+import Display from "components/Display/Display";
+import Button from "components/Button/Button";
 
 const App = () => {
-  const [ counter, setCounter ] = useState(9)
-  const changeCount = (delta) => setCounter(counter + delta)
+  let initialValue;
+  if (localStorage.getItem("counter") !== null)
+    initialValue = parseInt(localStorage.getItem("counter"));
+  else initialValue = 0;
+  const [counter, setCounter] = useState(initialValue);
+  const changeCount = (delta) => {
+    setCounter(counter + delta);
+    localStorage.setItem("counter", JSON.stringify(counter + delta));
+  };
 
   return (
     <div>
       <Display counter={counter} />
-      <Button
-        changeCount={changeCount} 
-        text='plus' 
-        delta={+1}
-      />
-      <Button 
-        changeCount={changeCount} 
-        text='zero'
-        delta={-counter}
-      />
-      <Button 
-        changeCount={changeCount} 
-        text='minus' 
-        delta={-1}
-      />
+      <Button changeCount={changeCount} text="plus" delta={+1} />
+      <Button changeCount={changeCount} text="zero" delta={-counter} />
+      <Button changeCount={changeCount} text="minus" delta={-1} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
